ef4dc42393fd08420bb16ae0cef01e83cc14faa5
Rick Waldron <waldron.rick@gmail.com>
2011-08-03 21:57:45 -0400

    Moves makefile kit into kits/makefile-kit

---
+++ kits/makefile-kit/src/foo.js
@@ -0,0 +1,14 @@
+/*!
+ * foo.js
+ * 
+ * Copyright (c) 2011 Your Name
+ * Dual licensed under the MIT and GPL licenses.
+ */
+
+(function( global ) {
+
+	global.foo = function() {
+		return true;
+	};
+
+})( this );
